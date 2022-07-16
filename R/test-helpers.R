8d78e5faafde7b2a4fe73934237332d64673929c
Russ Allen <rrjallen@yahoo.com>
2022-07-15 21:25:00 -0500

    added and reworked tests

--- R/test-helpers.R
+++ R/test-helpers.R
@@ -1,6 +1,7 @@
-# In the new version of the api, only three of the endpoints are searchable 
-# by patent number.  get_test_query() provides a sample query for each 
-# endpoint, except for locations, which isn't on the test server yet
+
+# I tried having this filename as helper-queries.R but the ubuntu-20.04 job failed 
+# while the other three jobs worked.  Serveral tests use get_test_query()
+# Feels a bit awkward...
 
 query_for_endpoint <- c(
       "application_citations" = '{"patent_number": "10966293"}',  # still searchable by pn
@@ -17,8 +18,29 @@ query_for_endpoint <- c(
       "uspc_mainclasses" = '{"uspc_mainclass_id":"30"}',
       "uspc_subclasses" = '{"uspc_subclass_id": "100/1"}')
 
-# Here so more than one test can use the queries
-
+#' Get Test Query
+#'  
+#' In the new version of the api, only three of the endpoints are searchable 
+#' by patent number. This function provides a sample query for each 
+#' endpoint, except for locations, which isn't on the test server yet
+#'
+#' @param endpoint The web service resource you want a test query for. \code{endpoint}
+#'  must be one of the following: "patents", "inventors", "assignees",
+#'  "locations", "cpc_groups", "cpc_subgroups", "cpc_subsections", "uspc_mainclasses",  
+#'  "uspc_subclasses","nber_categories", "nber_subcategories", "application_citations", 
+#'  or "patent_citations"
+#'
+#' @return a test query for the specified endpoint.
+#'
+#' @examples
+#'
+#' \dontrun{
+#'
+#'   get_test_query("patents")
+#'
+#' }
+#'
+#' @export
 get_test_query <- function (endpoint) {
 
    ifelse(endpoint %in% names(query_for_endpoint), query_for_endpoint[[endpoint]], NA)
