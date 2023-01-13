14161e2d90f1ce9056b64e6afb6d7e60a4940f89
Russ Allen <rrjallen@yahoo.com>
2023-01-13 11:33:46 -0600

    moved get_default_sort

--- R/get-fields.R
+++ R/get-fields.R
@@ -62,36 +62,10 @@ get_fields <- function(endpoint, groups = NULL) {
 #' @export
 get_endpoints <- function() {
   c(
-    "patent/us_application_citations", "assignees", "cpc_classes", "cpc_subclasses",
-    "cpc_groups", "inventors", "nber_categories",
-    "nber_subcategories", "patent/us_patent_citations", "patents",
-    "uspc_subclasses", "uspc_mainclasses",
+    "patent/us_application_citations", "assignees", "cpc_subclasses", "cpc_groups",
+    "cpc_classes", "inventors", "patent/us_patent_citations", "patents",
+    "uspc_subclasses", "uspc_mainclasses", "locations",
     "patent/attorneys", "patent/foreign_citations", "patent/rel_app_texts",
-    "locations", "wipo", "ipcs" #, patent/otherreference
+    "wipo", "ipcs"
   )
-}
-
-# Now for paging there has to be a sort field.  Here we'll provide defaults
-# for each endpoint if the user does not specify one
-
-# most of the nested endpoints use patent_id, patent/attorneys is the exception
-USE_PATENT_ID <- c("patent/us_application_citations", "patent/us_patent_citations", 
-"patent/rel_app_texts", "patent/foreign_citations")
-
-#' @noRd
-get_default_sort <- function(endpoint) {
-
-   if(endpoint %in% USE_PATENT_ID) {
-      key <- "patent_id"
-   }
-   else
-   {
-      key <- sub("^patent/", "", endpoint)
-      key <- to_singular(key)
-      key <- paste0(key,"_id")
-   }
-
-   default <- c("asc")
-   names(default) = key
-   default
 }
