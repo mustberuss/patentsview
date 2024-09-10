8b09d1005b6137e2c5605ae859f5673cb5ada30c
Russ Allen <rrjallen@yahoo.com>
2024-09-10 14:40:08 -0500

    fixed mismatched test

--- tests/testthat/test-api-bugs.R
+++ tests/testthat/test-api-bugs.R
@@ -13,30 +13,65 @@ test_that("API returns all requested groups", {
   skip_on_cran()
   skip_on_ci()
 
+  # can we traverse the return building a list of fields?
+  # sort both requested fields and returned ones to see if they are equal
+
+  # TODO: remove the trickery to get this test to pass, once the API is fixed
   bad_eps <- c("cpc_subclasses"
     , "location"        # Error: Invalid field: location_latitude
     , "uspc_subclasse"  # Error: Internal Server Error
     , "uspc_mainclass" # Error: Internal Server Error
-    , "wipo"            # Error: Internal Server Error
+    , "wipo"             # Error: Internal Server Error
     , "claim"           # Error: Invalid field: claim_dependent
     , "draw_desc_text"  # Error: Invalid field: description_sequence
     , "cpc_subclass"    # 404?  check the test query
     , "uspc_subclass"   # 404
    )
 
-  # for these endpoints not all the groups in the OpenAPI object are returned
   mismatched_returns <- c(
      "patent",
-     "publication",
-     "patent/us_application_citation",
-     "patent/us_patent_citation",
-     "patent/attorney",
-     "patent/foreign_citation",
-     "patent/rel_app_text",  # check these?
-     "publication/rel_app_text"
+     "publication"
   ) 
 
-  # this test will fail when the api is fixed
+  good_eps <- eps[!eps %in% bad_eps]
+  good_eps <- good_eps[!good_eps %in% mismatched_returns]
+
+  z <- lapply(good_eps, function(x) {
+    print(x)
+    res <- search_pv(
+      query = TEST_QUERIES[[x]],
+      endpoint = x,
+      fields = get_fields(x)
+    )
+
+    dl <- unnest_pv_data(res$data)
+
+    actual_groups <- names(dl)
+
+    expected_groups <- unique(fieldsdf[fieldsdf$endpoint == x, "group"])
+
+    # we now need to unnest the endpoints for the comparison to work
+    expected_groups <- gsub("^(patent|publication)/","", expected_groups)
+
+
+    # for "publication/rel_app_text" the expected group is really "rel_app_text_publications"
+    # which doesn't match the endpoint
+    if(x == "publication/rel_app_text") {
+       expected_groups <- replace(expected_groups, expected_groups=="", "rel_app_text_publications")
+    }
+    else
+    {
+       # the expected group for unnested attributes would be "" in actuality the come back
+       # in an entity matching the plural form of the unnested endpoint
+       expected_groups <- replace(expected_groups, expected_groups=="", to_plural(x))
+
+    }
+
+    expect_setequal(actual_groups, expected_groups)
+    show_failure(expect_setequal(actual_groups, expected_groups))
+  })
+
+  # this will fail when the api is fixed
   z <- lapply(bad_eps, function(x) {
     print(x)
     expect_error(
@@ -61,9 +96,9 @@ test_that("API returns all requested groups", {
     # we now need to unnest the endpoints for the comparison to work
     expected_groups <- gsub("^(patent|publication)/","", expected_groups)
 
-    # in the expected groups, the endpoint's group is singular in, it needs to be plural
-    # for the comparison to work
-    expected_groups <- replace(expected_groups, expected_groups==x, to_plural(x))
+    # the expected group for unnested attributes would be "" in actuality the come back
+    # in an entity matching the plural form of the unnested endpoint
+    expected_groups <- replace(expected_groups, expected_groups=="", to_plural(x))
 
     # better way to do this?  want to expect_set_not_equal
     expect_error(
