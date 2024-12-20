3daa5bd28e60ecc486657b9c94b4e81772d2dfec
Russ Allen <rrjallen@yahoo.com>
2024-12-20 09:38:00 -0600

    feat: new paging methodology

--- R/search-pv.R
+++ R/search-pv.R
@@ -74,7 +74,31 @@ patentsview_error_body <- function(resp) {
 
   resp
 }
+
+#' Pad patent_id
+#'
+#' This function strategically pads a patent_id with zeroes to 8 characters,
+#' needed only for custom paging that uses sorts by patent_id.
+#'
+#' @param patent_id The patent_id that needs to be padded.  It can
+#' be the patent_id for a utility, design, plant or reissue patent.
+#'
+#' @examples
+#' \dontrun{
+#' padded <- pad_patent_id("RE36479")
+#'
+#' padded2 <- pad_patent_id("3930306")
+#' }
+#'
+#' @export
+# zero pad patent_id to 8 characters.
+pad_patent_id <- function(patent_id) {
+  pad <- 8 - nchar(patent_id)
+  if (pad > 0) {
+    patent_id <- paste0(sprintf("%0*d", pad, 0), patent_id)
+    patent_id <- sub("(0+)([[:alpha:]]+)([[:digit:]]+)", "\\2\\1\\3", patent_id)
   }
+  patent_id
 }
 
 #' @noRd
@@ -103,10 +127,30 @@ request_apply <- function(ex_res, method, query, base_url, arg_list, api_key, ..
     arg_list$opts$offset <- (i - 1) * arg_list$opts$size
     x <- one_request(method, query, base_url, arg_list, api_key, ...)
     x <- process_resp(x)
+
+    # now to page we need to set the "after" attribute to where we left off
+    # we want the last value of the primary sort field
+    s <- names(arg_list$sort[[1]])[[1]]
+    index <- nrow(x$data[[1]])
+    last_value <- x$data[[1]][[s]][[index]]
+
+    if (s == "patent_id") {
+      last_value <- pad_patent_id(last_value)
+    }
+
+    arg_list$opts$after <<- last_value
+
     x$data[[1]]
   })
 
   do.call("rbind", c(tmp, make.row.names = FALSE))
+}
+
+#' @noRd
+get_default_sort <- function(endpoint) {
+  default <- c("asc")
+  names(default) <- get_ok_pk(endpoint)
+  default
 }
 
 #' Search PatentsView
@@ -244,11 +288,62 @@ search_pv <- function(query,
 
   result <- one_request(method, query, base_url, arg_list, api_key, ...)
   result <- process_resp(result)
-  if (!all_pages) return(result)
 
-  full_data <- request_apply(result, method, query, base_url, arg_list, api_key, ...)
-  result$data[[1]] <- full_data
+  if (all_pages && result$query_result$total_hits == 0) {
+    stop2("No records matched your query...Can't download multiple pages")
+  }
 
+  # return if we don't need to make additional API requests
+  if (!all_pages ||
+    result$query_result$total_hits == 0 ||
+    result$query_result$total_hits == nrow(result$data[[1]])) {
+    return(result)
+  }
+
+  # Here we ignore the user's sort and instead have the API sort by the primary
+  # key for the requested endpoint.  
+  primary_sort_key <- get_default_sort(endpoint)
+
+  # We check what fields we got back from the first call. If the user didn't
+  # specify fields, we'd get back the API's defaults.  We may need to request
+  # additional fields from the API so we can apply the users sort and then remove
+  # the additional fields.
+  returned_fields <- names(result$data[[1]])
+
+  if (!is.null(sort)) {
+    sort_fields <- names(sort)
+    additional_fields <- sort_fields[!(sort_fields %in% returned_fields)]
+    if (is.null(fields)) {
+      fields <- returned_fields # the default fields
+    } else {
+      fields <- fields # user passed
+    }
+    fields <- append(fields, additional_fields)
+  } else {
+    additional_fields <- c()
+  }
+
+  arg_list <- to_arglist(fields, size, primary_sort_key, after)
+  paged_data <- request_apply(result, method, query, base_url, arg_list, api_key, ...)
+
+  # apply the user's sort using order()
+  # was data.table::setorderv(paged_data, names(sort), ifelse(as.vector(sort) == "asc", 1, -1))
+
+  sort_order <- mapply(function(col, direction) {
+    if (direction == "asc") {
+      return(paged_data[[col]])
+    } else {
+      return(-rank(paged_data[[col]], ties.method = "min"))  # Invert for descending order
+    }
+  }, col = names(sort), direction = as.vector(sort), SIMPLIFY = FALSE)
+
+  # Final sorting
+  paged_data <- paged_data[do.call(order, sort_order), , drop = FALSE]
+
+  # remove the fields we added in order to do the user's sort ourselves
+  paged_data <- paged_data[, !names(paged_data) %in% additional_fields]
+
+  result$data[[1]] <- paged_data
   result
 }
 
