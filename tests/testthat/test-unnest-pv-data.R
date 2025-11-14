b25d6bd4812827440cf0259d99ec1d6b574704f0
Russ Allen <rrjallen@yahoo.com>
2025-11-13 22:37:17 -0600

    moved code to api-bugs

--- tests/testthat/test-unnest-pv-data.R
+++ tests/testthat/test-unnest-pv-data.R
@@ -17,23 +17,4 @@ test_that("we can unnest all entities", {
     unnested <- unnest_pv_data(pv_out[["data"]])
     expect_is(unnested, "list")
   })
-
-  # mismatch in actually returned fields vs expected fields
-  dev_null <- lapply(troubled_endpoints, function(x) {
-    print(x)
-    pv_out <- search_pv(
-      query = TEST_QUERIES[[x]],
-      endpoint = x,
-      fields = get_fields(x)
-    )
-    actual_fields <- colnames(pv_out$data[[1]])
-
-    this_ep_fields <- fieldsdf[fieldsdf$endpoint == x, "field"]
-    this_ep_grps <- unique(fieldsdf[fieldsdf$endpoint == x, "group"])
-    expected_fields = c(this_ep_fields, this_ep_grps)
-    left <- all(actual_fields %in% expected_fields)
-    right <- all(expected_fields %in% actual_fields)
-    expect_false(all(left, right))
-  })
-
 })
